1b76e5c5ee20b6e71f7618ca8b26bf5cc097ed9d
ibraheemsiddqui928-spec <ibraheemsiddqui928@gmail.com>
2026-09-21 00:55:24 +0500

    .

---
+++ script.js
@@ -0,0 +1,9 @@
+let count = 0;
+let totalCount = 0;
+let laps = 0;
+let soundEnable = true;
+
+const countDisplay = document.getElementById('countDisplay');
+const totalCountDisplay = document.getElementById('totalCount');
+const lapCountDisplay = document.getElementById('lapCount');
+const progressText = document.getElementById('progressText');
